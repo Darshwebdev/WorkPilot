7190e37f0e01168890f0cdede8ad36832eb9e9bd
Darsh Desai <darshdesai2504@gmail.com>
2025-06-29 16:58:11 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { base } from 'motion/react-client'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
      tailwindcss(),
-    react()
+    react(),
   ],
+  base:"/WorkPilot",
 })
